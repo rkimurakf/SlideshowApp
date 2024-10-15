1c9af6d3cfadc2a934f59a1c2b01923af0ef43d5
ryuuto.kimura <r_kimura@kyoei-e.com>
2024-10-15 13:27:49 +0900

    1015

---
+++ SlideshowApp/ImageViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ImageViewController.swift
+//  SlideshowApp
+//
+//  Created by mba2408.silver kyoei.engine on 2024/10/10.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+    
+    @IBOutlet weak var sendView: UIImageView!
+    var receivedImage: UIImage?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let image = receivedImage{
+            sendView.image = image
+            // Do any additional setup after loading the view.
+            sendView.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+        }
+        
+        
+    }
+    
+    @IBAction func close(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
+/*
+ // MARK: - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ // Get the new view controller using segue.destination.
+ // Pass the selected object to the new view controller.
+ 
+ */
--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -8,12 +8,114 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageVIEW: UIImageView!
+    @IBOutlet weak var startButton: UIButton!
+    
+    @IBOutlet weak var up: UIButton!
+    @IBOutlet weak var down: UIButton!
+    var hantei: Bool = false
+    
+    //最初に表示させる画像
+    var page:Int = 0
+    
+    var timer:Timer!
+    
+    var imageArray:[UIImage] = [
+        UIImage(named: "inu3")!,
+        UIImage(named: "inu2")!,
+        UIImage(named: "inu1")!,
+        UIImage(named: "inu4")!
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func tapgesture(_ sender: Any) {
+        
+    }
+    
+    @IBAction func Abutton(_ sender: Any) {
+        if hantei == false {
+            //ONにする時に走らせたい処理
+            startButton.setTitle("停止", for: .normal)
+            hantei = true
+            timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(changeImage), userInfo: nil, repeats: true)
+            
+            up.isEnabled = false
+            down.isEnabled = false
+            
+        } else if hantei == true {
+            //OFFにする時に走らせたい処理
+            startButton.setTitle("開始", for: .normal)
+            hantei = false
+            up.isEnabled = true
+            down.isEnabled = true
+            timer.invalidate()
+        }
+        
+    }
+    
+    
+    
+    //プラス１関数
+    @objc func changeImage(){
+        page += 1
+        if(page  == imageArray.count){
+            page = 0
+        }
+        imageVIEW.image = imageArray[page]
+    }
+    //進むボタン
+    @IBAction func up(_ sender: Any) {
+        if(timer == nil){
+            page += 1
+        }else{
+            timer.invalidate()
+            startButton.setTitle("開始", for: .normal)
+            hantei = false
+            page += 1
+        }
+        if(page  == imageArray.count){
+            page = 0
+        }
+        imageVIEW.image = imageArray[page]
+    }
+    
+    
+    
+    //戻るボタン
+    @IBAction func down(_ sender: Any) {
+        if(timer == nil){
+            page -= 1
+        }else{
+            timer.invalidate()
+            startButton.setTitle("開始", for: .normal)
+            hantei = false
+            page -= 1
+        }
+        if page == -1 {
+            page = 3
+        }
+        
+        imageVIEW.image = imageArray[page]
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue,sender: Any?){
+        if segue.identifier == "showImageDetail"{
+            let destinationVC = segue.destination as! ImageViewController
+            destinationVC.receivedImage = imageVIEW.image
+            startButton.setTitle("開始", for: .normal)
+            up.isEnabled = true
+            down.isEnabled = true
+            hantei = false
+            timer.invalidate()
+        }
+    }
+    
+    
 }
+
 
